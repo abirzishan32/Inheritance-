6f5ccc3b3dcd1ee3a5ac7b91e308e22fbc62c5f0
abir_zishan <118625188+abirzishan32@users.noreply.github.com>
2023-04-03 11:52:44 +0600

    Create Overloading Unary Operator.cpp

---
+++ Overloading Unary Operator.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define pb push_back
+#define N 10000005
+#define div 1000003
+#define mod 1000000007
+#define MOD 998244353
+#define fr(i,a,b) for(int i = a;i<b;i++)
+#define fri(i,a,b) for(int i = a;i>=b;i--)
+#define pyn(flag) cout<<(flag?"YES":"NO")<<endl;
+#define vrev reverse(v.begin(), v.end())
+#define vin cin>>v[i]
+#define vsort sort(v.begin(), v.end())
+#define yout cout<<"YES"<<endl
+#define nout cout<<"NO"<<endl;
+
+class A {
+    private:
+    int num;
+    public:
+    A() : num(0) {}
+    A(int x) : num(x) {}
+
+
+    A operator - (){
+        num = -num;
+    }
+
+    void getNum(){
+        cout << num<<endl;
+    }
+};
+
+
+int main(int argc, const char** argv) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    A obj(10);
+    obj.getNum();
+    -obj;
+    obj.getNum();
+    return 0;
+}
