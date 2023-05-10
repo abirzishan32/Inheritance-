7b6a8b5943ae4725d985a0eb9b251d872151e703
abir_zishan <118625188+abirzishan32@users.noreply.github.com>
2023-05-11 02:29:44 +0600

    Create Exception Handling - 3.cpp

---
+++ Exception Handling - 3.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Nested try-catch blocks
+    
+int main() {
+    int n;
+    cin>>n;
+    try{    //Outer Try
+        try{        //Inner Try
+            if(n==1){       //Throwing Exceptions
+                throw 1;
+            }
+            else if(n==2){      //Throwing Exceptions
+                throw 2;
+            }
+            else if(n==3){      //Throwing Exceptions
+                throw 3;
+            }
+            else{       //Throwing Exceptions 
+                throw 4;
+            }
+        }
+        catch(int n){       //Inner Catch
+            cout<<"Inner Catch "<<n<<endl;
+            throw;      //Re-Throwing Exceptions
+        }       
+    }
+    catch(int n){       //Outer Catch
+        cout<<"Outer Catch "<<n<<endl;
+    }       
+    return 0;
+}
