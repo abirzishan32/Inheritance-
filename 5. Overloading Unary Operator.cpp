7fed2aa54efea6276459345437749acdb803c485
abir_zishan <118625188+abirzishan32@users.noreply.github.com>
2023-05-17 01:18:40 +0600

    Update 5. Overloading Unary Operator.cpp

--- 5. Overloading Unary Operator.cpp
+++ 5. Overloading Unary Operator.cpp
@@ -1,28 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef pair<string, string> pss;
-typedef vector<int> vi;
-typedef vector<vi> vvi;
-typedef vector<pii> vii;
-typedef vector<ll> vl;
-typedef vector<vl> vvl;
 
-#define pb push_back
-#define N 10000005
-#define div 1000003
-#define mod 1000000007
-#define MOD 998244353
-#define fr(i,a,b) for(int i = a;i<b;i++)
-#define fri(i,a,b) for(int i = a;i>=b;i--)
-#define pyn(flag) cout<<(flag?"YES":"NO")<<endl;
-#define vrev reverse(v.begin(), v.end())
-#define vin cin>>v[i]
-#define vsort sort(v.begin(), v.end())
-#define yout cout<<"YES"<<endl
-#define nout cout<<"NO"<<endl;
 
 class A {
     private:
@@ -33,7 +11,9 @@ class A {
 
 
     A operator - (){
-        num = -num;
+        A temp;
+        temp.val = -val;
+        return temp;
     }
 
     A friend operator - (A obj){
